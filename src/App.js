9cde34ea3e2e7a8648894a35c4be240e83ddf99d
Kirubhanandhan Raja <ki20263691@wipro.com>
2022-06-01 15:54:24 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import './App.css';
+import {Assignment1,Assignment2} from './Assignment/Assignment1';
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        
+          <Assignment1 />
+          <Assignment2/>
+     
+      </div>
+
+    );
+  }
+}
+
+
---
+++ src/Assignment/Assignment1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+export  class Assignment1 extends React.Component{
+render(){
+    return(
+       <h1>Happy Learning-React</h1>
+    );
+}
+}
+export  class Assignment2 extends React.Component{
+    render(){
+        return(
+           <h1>Happy Learning-React-kirubha</h1>
+        );
+    }
+    }
